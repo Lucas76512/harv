303cdaf04b3b937c183a54f6b3dbd6dd0fae93b2
Lucassantoss <lucas@buildbox.com.br>
2023-08-20 13:55:48 -0300

    adição de novos arquivos

--- cypress/e2e/produto-service.cy.js
+++ cypress/e2e/produto-service.cy.js
@@ -21,24 +21,17 @@ describe('Cadastro produto  ', () => {
             cy.get('.sc-edUIhV > .sc-bjuIDw').as('frete').click()
             cy.get(':nth-child(20) > .sc-kItwNn > .sc-bSakgD > .sc-jQHtVU > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
             cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
-            cy.get(':nth-child(21) > .sc-cUEIKg > .sc-bTmccw').as('entrega programada').click()
-            cy.get('.sc-kItwNn > .sc-bSakgD > .haOJaf > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-            cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
+            cy.get(':nth-child(20) > .sc-cUEIKg > .sc-bTmccw').click();
 
-            cy.get('.sc-kItwNn > .sc-bSakgD > :nth-child(2) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-            cy.get('.MuiAutocomplete-popper li[data-option-index="12"]').click();
+            cy.get('.sc-gDeeJ > .MuiButtonBase-root').click();
+            
+            cy.url().should('be.equal', 'https://harv.buildbox.one/#/seller/products');
+            cy.contains('span','Produto Cadastrado!').should('be.visible')
 
-            cy.get(':nth-child(3) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-            cy.get('.MuiAutocomplete-popper li[data-option-index="32"]').click();
-            cy.get(':nth-child(20) > .sc-cUEIKg > .sc-bTmccw').click()
-
-            cy.visit('https://harv.buildbox.one/#/seller/create/product')
-  
-            cy.contains('sucess')
-              .should('not.exist')
 
         })
-        it.only('redirecionamento para tela de listagem de produtos', () => {
+
+        it.only('Cadastro de produto com entrega programada', () => {
            
             cy.get(':nth-child(2) > .sc-idiyUo > .sc-kgflAQ').type('nikolas@buildbox.com.br')
             cy.get(':nth-child(3) > .sc-idiyUo > .sc-kgflAQ').type('Teste321#')
@@ -56,6 +49,42 @@ describe('Cadastro produto  ', () => {
             cy.get('input[name="quantity"]').as('quantity').type('1')
             cy.get('input[name="price"]').as('price').type('15')
             cy.get('input[name="discount"]').as('discount').type('10')
+
+
+            cy.get(':nth-child(21) > .sc-cUEIKg > .sc-bTmccw').as('entrega programada').click()
+            cy.get('.sc-kItwNn > .sc-bSakgD > .haOJaf > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+            cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
+
+            cy.get('.sc-kItwNn > .sc-bSakgD > :nth-child(2) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+            cy.get('.MuiAutocomplete-popper li[data-option-index="12"]').click();
+
+            cy.get(':nth-child(3) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+            cy.get('.MuiAutocomplete-popper li[data-option-index="32"]').click();
+            cy.get('.sc-gDeeJ > .MuiButtonBase-root').click();
+
+            cy.url().should('be.equal', 'https://harv.buildbox.one/#/seller/products');
+            cy.contains('span','Produto Cadastrado!').should('be.visible')
+
+
+        })
+        it.only('redirecionamento para tela de listagem de produtos ', () => {
+           
+            cy.get(':nth-child(2) > .sc-idiyUo > .sc-kgflAQ').type('nikolas@buildbox.com.br')
+            cy.get(':nth-child(3) > .sc-idiyUo > .sc-kgflAQ').type('Teste321#')
+            cy.get('button[label="Entrar"]').click()
+            cy.get('button[label="Novo Produto"]').click()
+            
+            cy.get('input[name="video"]').selectFile('cypress/e2e/fixtures/video.mp4', { force: true })
+            cy.get('input[name="image_01"]').selectFile('cypress/e2e/fixtures/sorvete.jpg', { force: true })
+            cy.get('input[name="name"]').as('name').type('Sorvete')
+            cy.get(':nth-child(9) > .sc-jQHtVU > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+            cy.get('.MuiAutocomplete-popper li[data-option-index="8"]').click();
+        
+            cy.get('.sc-eGAhfa').as('desciption').type('Sorvete')
+            cy.get('input[name="weight"]').as('weight').type('400g')
+            cy.get('input[name="quantity"]').as('quantity').type('1')
+            cy.get('input[name="price"]').as('price').type('15')
+            cy.get('input[name="discount"]').as('discount').type('10')
             cy.get('input[name="start_Date"]').type('2023-08-17')
             cy.get('input[name="end_Date"]').invoke('removeAttr', 'disabled').type('2023-08-20')
             cy.get('.sc-egNfGp > .sc-bSakgD > :nth-child(2) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
@@ -66,23 +95,64 @@ describe('Cadastro produto  ', () => {
             cy.get('.sc-edUIhV > .sc-bjuIDw').as('frete').click()
             cy.get(':nth-child(20) > .sc-kItwNn > .sc-bSakgD > .sc-jQHtVU > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
             cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
-            cy.get(':nth-child(21) > .sc-cUEIKg > .sc-bTmccw').as('entrega programada').click()
-            cy.get('.sc-kItwNn > .sc-bSakgD > .haOJaf > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-            cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
-            cy.get('input[name="start_Date"]').type('2023-08-17')
-            cy.get('input[name="end_Date"]').invoke('removeAttr', 'disabled').type('2023-08-20') 
 
+            cy.get('input[name="start_Date"]').type('2023-08-20')
+            cy.get('input[name="end_Date"]').invoke('removeAttr', 'disabled').type('2023-08-23') 
+        
+        
 
-            cy.get('.sc-kItwNn > .sc-bSakgD > :nth-child(2) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-            cy.get('.MuiAutocomplete-popper li[data-option-index="12"]').click();
-
-            cy.get(':nth-child(3) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-            cy.get('.MuiAutocomplete-popper li[data-option-index="32"]').click();
             cy.get('.sc-gDeeJ > .MuiButtonBase-root').click();
-
+        
             cy.url()
             .should('be.equal', 'https://harv.buildbox.one/#/seller/products')
-        })
+      
+})
+
+it.only('redirecionamento para tela de listagem de produtos com entrega programada ', () => {
+           
+    cy.get(':nth-child(2) > .sc-idiyUo > .sc-kgflAQ').type('nikolas@buildbox.com.br')
+    cy.get(':nth-child(3) > .sc-idiyUo > .sc-kgflAQ').type('Teste321#')
+    cy.get('button[label="Entrar"]').click()
+    cy.get('button[label="Novo Produto"]').click()
+    
+    cy.get('input[name="video"]').selectFile('cypress/e2e/fixtures/video.mp4', { force: true })
+    cy.get('input[name="image_01"]').selectFile('cypress/e2e/fixtures/sorvete.jpg', { force: true })
+    cy.get('input[name="name"]').as('name').type('Sorvete')
+    cy.get(':nth-child(9) > .sc-jQHtVU > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+    cy.get('.MuiAutocomplete-popper li[data-option-index="8"]').click();
+
+    cy.get('.sc-eGAhfa').as('desciption').type('Sorvete')
+    cy.get('input[name="weight"]').as('weight').type('400g')
+    cy.get('input[name="quantity"]').as('quantity').type('1')
+    cy.get('input[name="price"]').as('price').type('15')
+    cy.get('input[name="discount"]').as('discount').type('10')
+    cy.get('input[name="start_Date"]').type('2023-08-17')
+    cy.get('input[name="end_Date"]').invoke('removeAttr', 'disabled').type('2023-08-20')
+    cy.get('.sc-egNfGp > .sc-bSakgD > :nth-child(2) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+    cy.get('.MuiAutocomplete-popper li[data-option-index="12"]').click();
+    
+    cy.get(':nth-child(4) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+    cy.get('.MuiAutocomplete-popper li[data-option-index="12"]').click();
+    cy.get('.sc-edUIhV > .sc-bjuIDw').as('frete').click()
+ 
+    cy.get(':nth-child(21) > .sc-cUEIKg > .sc-bTmccw').as('entrega programada').click()
+    cy.get('.sc-kItwNn > .sc-bSakgD > .haOJaf > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+    cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
+    cy.get('input[name="start_Date"]').type('2023-08-20')
+    cy.get('input[name="end_Date"]').invoke('removeAttr', 'disabled').type('2023-08-23') 
+
+
+    cy.get('.sc-kItwNn > .sc-bSakgD > :nth-child(2) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+    cy.get('.MuiAutocomplete-popper li[data-option-index="12"]').click();
+
+    cy.get(':nth-child(3) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+    cy.get('.MuiAutocomplete-popper li[data-option-index="32"]').click();
+    cy.get('.sc-gDeeJ > .MuiButtonBase-root').click();
+
+    cy.url()
+    .should('be.equal', 'https://harv.buildbox.one/#/seller/products')
+
+})
 })
 
 describe('Cadastro de serviço', () => {
--- cypress/e2e/seller.cy.js
+++ cypress/e2e/seller.cy.js
@@ -7,6 +7,61 @@ describe('Tests', () => {
         cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
 
         cy.get('input[name="commercial_phone"]').as('commercial_phone').type('18988241771')  
+        cy.get('input[name="document"]').as('document').type('29.109.344/0001-15')
+        cy.get('input[name="corporate_name"]').as('corporate_name').type('Exemplo Brasil LTDA', {force: true})
+        cy.get('input[name="email"]').as('email').type('lucas+7@buildbox.com.br')
+        cy.get('input[name="confirm_email"]').as('confirm_email').type('lucas+7@buildbox.com.br')
+        cy.get('input[name="store_name"]').as('store_name').type('teste')
+        cy.get('textarea[name="store_bio"]').type('Segunda a Sexta 05:00 até 17:00')
+        cy.get('input[name="cep"]').as('cep').type('13049-024')
+        cy.get('input[name="number"]').as('number').type('1223')
+        cy.get('input[name="district"]').as('district').type('Jardim Monte Cristo')
+        cy.get('input[name="complement"]').as('complement').type('casa')
+
+        cy.get(':nth-child(2) > .sc-ehmTmK > .sc-gFGZVQ > :nth-child(1) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
+
+        cy.get(':nth-child(2) > .sc-ehmTmK > .sc-gFGZVQ > :nth-child(2) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
+
+        cy.get(':nth-child(3) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
+
+        cy.get(':nth-child(4) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
+
+        cy.get(':nth-child(17) > .sc-jQHtVU > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
+ 
+        cy.get('input[name="agency"]').as('agency').type('1234')
+        cy.get('input[name="account_number"]').as('account_number').type('40028922')
+        cy.get('input[name="account_digit"]').as('account_digit').type('d')
+        cy.get('input[name="account_name"]').as('account_name').type('Pedro Henrique')
+        cy.get('input[name="account_document"]').as('account_document').type('697.777.790-70', {force: true} )
+
+        cy.get(':nth-child(19) > .sc-jQHtVU > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
+        cy.get('input[name="first_name"]').as('first_name').type('Pedro')
+        cy.get('input[name="last_name"]').as('last_name').type('Henrique')
+        cy.get('input[name="cpf"]').as('cpf').type('569.306.390-95')
+        cy.get('input[name="phone"]').as('phone').type('4523462354')
+        cy.get('input[name="password"]').as('password').type('Teste321#')
+        cy.get('input[name="confirm_password"]').as('confirm_password').type('Teste321#')
+        cy.get('.sc-TRNrF > .MuiButtonBase-root').click()
+        
+        cy.url().should('be.equal', 'https://harv.buildbox.one/#/seller/success-registration')
+        cy.contains('span','Seller cadastrado com sucesso!').should('be.visible')
+
+    })
+
+    it.only('EMAIL test', () => {
+
+        cy.get('input[name="photo"]').selectFile('cypress/e2e/fixtures/sorvete.jpg', { force: true })
+
+        cy.get(':nth-child(4) > .sc-jQHtVU > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
+        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
+
+        cy.get('input[name="commercial_phone"]').as('commercial_phone').type('18988241771')  
         cy.get('input[name="document"]').as('document').type('54.925.830/0001-59')  
         cy.get('input[name="corporate_name"]').as('corporate_name').type('Exemplo Brasil LTDA', {force: true})
         cy.get('input[name="email"]').as('email').type('lucas+6@buildbox.com.br')
@@ -48,63 +103,8 @@ describe('Tests', () => {
         cy.get('input[name="password"]').as('password').type('Teste321#')
         cy.get('input[name="confirm_password"]').as('confirm_password').type('Teste321#')
         cy.get('.sc-TRNrF > .MuiButtonBase-root').click()
-        
-        cy.url().should('be.equal', 'https://harv.buildbox.one/#/seller/success-registration')
-        cy.contains('span','Seller cadastrado com sucesso!').should('be.visible')
 
-    })
-
-    it.only('EMAIL test', () => {
-
-        cy.get('input[name="photo"]').selectFile('cypress/e2e/fixtures/sorvete.jpg', { force: true })
-
-        cy.get(':nth-child(4) > .sc-jQHtVU > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
-
-        cy.get('input[name="commercial_phone"]').as('commercial_phone').type('18988241771')  
-        cy.get('input[name="document"]').as('document').type('53.567.598/0001-61')  
-        cy.get('input[name="corporate_name"]').as('corporate_name').type('Exemplo Brasil LTDA', {force: true})
-        cy.get('input[name="email"]').as('email').type('nikolassanntoss+1@icloud.com')
-        cy.get('input[name="confirm_email"]').as('confirm_email').type('nikolassanntoss+1@icloud.com')
-        cy.get('input[name="store_name"]').as('store_name').type('teste')
-        cy.get('textarea[name="store_bio"]').type('Segunda a Sexta 05:00 até 17:00')
-        cy.get('input[name="cep"]').as('cep').type('13049-024')
-        cy.get('input[name="number"]').as('number').type('1223')
-        cy.get('input[name="district"]').as('district').type('Jardim Monte Cristo')
-        cy.get('input[name="complement"]').as('complement').type('casa')
-
-        cy.get(':nth-child(2) > .sc-ehmTmK > .sc-gFGZVQ > :nth-child(1) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
-
-        cy.get(':nth-child(2) > .sc-ehmTmK > .sc-gFGZVQ > :nth-child(2) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
-
-        cy.get(':nth-child(3) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
-
-        cy.get(':nth-child(4) > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
-
-        cy.get(':nth-child(17) > .sc-jQHtVU > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
- 
-        cy.get('input[name="agency"]').as('agency').type('1234')
-        cy.get('input[name="account_number"]').as('account_number').type('40028922')
-        cy.get('input[name="account_digit"]').as('account_digit').type('d')
-        cy.get('input[name="account_name"]').as('account_name').type('Pedro Henrique')
-        cy.get('input[name="account_document"]').as('account_document').type('697.777.790-70', {force: true} )
-
-        cy.get(':nth-child(19) > .sc-jQHtVU > .MuiAutocomplete-root > .MuiFormControl-root > .MuiInputBase-root').click()
-        cy.get('.MuiAutocomplete-popper li[data-option-index="0"]').click();
-        cy.get('input[name="first_name"]').as('first_name').type('Pedro')
-        cy.get('input[name="last_name"]').as('last_name').type('Henrique')
-        cy.get('input[name="cpf"]').as('cpf').type('769.127.390-83')
-        cy.get('input[name="phone"]').as('phone').type('4523462354')
-        cy.get('input[name="password"]').as('password').type('Teste321#')
-        cy.get('input[name="confirm_password"]').as('confirm_password').type('Teste321#')
-        cy.get('.sc-TRNrF > .MuiButtonBase-root').click()
-        
-        cy.get('.foo').should('not.exist')
+        cy.contains('span', 'Usuário com esse CPF ou E-mail já cadastrado.').should('be.visible')
 
     })
     it.only('CPF test', () => {
@@ -157,7 +157,7 @@ describe('Tests', () => {
         cy.get('input[name="confirm_password"]').as('confirm_password').type('Teste321#')
         cy.get('.sc-TRNrF > .MuiButtonBase-root').click()
         
-        cy.get('.foo').should('not.exist')
+        cy.contains('span', 'Usuário com esse CPF ou E-mail já cadastrado.').should('be.visible')
 
     })
     it.only('CNPJ test', () => {
@@ -210,7 +210,7 @@ describe('Tests', () => {
         cy.get('input[name="confirm_password"]').as('confirm_password').type('Teste321#')
         cy.get('.sc-TRNrF > .MuiButtonBase-root').click()
         
-        cy.get('.foo').should('not.exist')
+        cy.contains('span', 'O CNPJ já possui uma loja cadastrada.').should('be.visible')
 
     })  
     
